7eea562c3a098bb03646533a51a51d30eebf7056
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-05-30 18:37:12 +1200

    Revert

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Version 12
+
+// Compass Code and alpha data etc inspired and adapted from HTML5 for the Mobile Web: Device Orientation Events
+// https://mobiforge.com/design-development/html5-mobile-web-device-orientation-events
+
+// Code also an combination of many helpful tutorials online but no major code taken just used to fix small issues
+
 function compass() {
   // Check for support for DeviceOrientation event and executes if the is support
   if(window.DeviceOrientationEvent) {
@@ -6,8 +13,23 @@ function compass() {
       var alpha; // Variable holder for alpha as it has different applications over different devises
       var nArrow = document.getElementById('north');
 
-      alpha = event.alpha; // Sets alpha for Andriod
-      webkitAlpha = alpha; // To be used for the chrome
+      // Check for iOS properties
+      if(event.webkitCompassHeading) {
+        alpha = event.webkitCompassHeading; // Calculates where North is for iPhone.
+        //Rotation is reversed for iOS
+        nArrow.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
+      }
+
+      // Non iOS.
+      else {
+        alpha = event.alpha; // Sets alpha for Andriod
+        webkitAlpha = alpha; // To be used for the chrome
+
+        //Assume Android stock (this is crude, will reccomend change to chrome) and apply offset
+        if(!window.chrome) {
+          webkitAlpha = alpha - 270;
+        }
+      }
 
       //Displaying of the data as a compass
       nArrow.style.Transform = 'rotate(' + alpha + 'deg)';
